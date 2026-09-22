fb05a025ee6fad4b1a85a07b35e123c57ddf4345
Andrey Evstigneev <evandvlad@gmail.com>
2026-09-22 20:42:33 +0400

    improve theme & styles

--- src/htmlx/components/table.ts
+++ src/htmlx/components/table.ts
@@ -6,9 +6,11 @@ export const table: HtmlxComponents["table"] = (props) => {
 	const { columns, rows } = props;
 
 	return `
-		<table ${stringifyCompAttrs({ compClass: "c_table", props })}>
-			${columns ? `<tr>${columns.map((value) => `<th>${value}</th>`)}</tr>` : ""}
-			${rows.map((row) => `<tr>${row.map((value) => `<td>${value}</td>`)}</tr>`)}
-		</table>
+		<div ${stringifyCompAttrs({ compClass: "c_table", props })}>
+			<table>
+				${columns ? `<tr>${columns.map((value) => `<th>${value}</th>`).join("")}</tr>` : ""}
+				${rows.map((row) => `<tr>${row.map((value) => `<td>${value}</td>`).join("")}</tr>`).join("")}
+			</table>
+		</div>
 	`;
 };
